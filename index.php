661e524c609b911697a99a93e8002530d8cbc428
Edy Galdiolo <edygaldy302@gmail.com>
2023-06-21 16:43:03 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+require './models/products.php';
+require './models/animal.php';
+require './models/filter.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>e-commerce</title>
+</head>
+<body>
+    
+</body>
+</html>
